e4dd8868f2847749a553bde580a605bd7eb2136b
jrquadros <quadrosjunioradits@gmail.com>
2019-06-25 22:14:38 -0300

    app init

---
+++ app.ts
@@ -0,0 +1,40 @@
+import * as express from "express";
+import * as graphqlHTTP from "express-graphql";
+import * as cors from "cors";
+
+import db from "./models";
+import schema from "./graphql/schema";
+import { extractJWTMiddleware } from "./graphql/middlewares/extract-jwt.middeware";
+
+class App {
+  public express: express.Application;
+
+  constructor() {
+    this.express = express();
+    this.middleware();
+  }
+
+  private middleware(): void {
+    //cors habilitado
+    this.express.use(cors());
+
+    this.express.use(
+      "/graphql",
+
+      extractJWTMiddleware(),
+
+      (req, res, next) => {
+        req["context"].db = db;
+        next();
+      },
+
+      graphqlHTTP(req => ({
+        schema: schema,
+        graphiql: true, //process.env.NODE_ENV === 'develpment',
+        context: req["context"]
+      }))
+    );
+  }
+}
+
+export default new App().express;
